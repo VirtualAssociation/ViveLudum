fc6a7d2da9aa5ce4da9125857a0eb02a4467c11a
Bastien Laby <bastien.laby@gmail.com>
2016-04-16 13:04:36 +0200

    add PNJ prefab with color param

---
+++ Assets/Scripts/PNJ.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PNJ : MonoBehaviour {
+
+    public Color Color;
+
+	// Use this for initialization
+	void Start () {
+        Renderer renderer = this.GetComponent<Renderer>();
+        renderer.material.color = Color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
