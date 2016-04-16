852d9a4f73544e77df73bffddb2031a1afd18dac
Guillaume Laruelle <g.laruelle@student.isartdigital.com>
2016-04-16 20:06:02 +0200

    Minor Fix

--- Assets/Scripts/PNJsController.cs
+++ Assets/Scripts/PNJsController.cs
@@ -7,7 +7,7 @@ public class PNJsController : MonoBehaviour {
     {
         int pnjId = Random.Range(0, Pnjs.Length);
         ShapeShift sh = Pnjs[pnjId].GetComponent<ShapeShift>();
-        sh.ChangeMesh(1);
+        //sh.ChangeMesh(1);
     }
 }
 
