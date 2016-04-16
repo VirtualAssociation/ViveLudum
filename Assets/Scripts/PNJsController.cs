41061b4e75d34dad369abb8d328e3da408361bdd
Bastien Laby <bastien.laby@gmail.com>
2016-04-16 20:28:21 +0200

    Update PNJsController

--- Assets/Scripts/PNJsController.cs
+++ Assets/Scripts/PNJsController.cs
@@ -1,13 +1,24 @@
 ﻿using UnityEngine;
 
 public class PNJsController : MonoBehaviour {
+    [SerializeField]
+    private GameObject[] characters;
+
     public GameObject[] Pnjs { get { return GameObject.FindGameObjectsWithTag("Destructible"); } }
+
+    void Update()
+    {
+        if (Input.GetButtonUp("ChangeForm"))
+        {
+            ShapeShift();
+        }
+    }
 
     public void ShapeShift()
     {
         int pnjId = Random.Range(0, Pnjs.Length);
         ShapeShift sh = Pnjs[pnjId].GetComponent<ShapeShift>();
-        //sh.ChangeMesh(1);
+        sh.ChangeMesh(characters[Random.Range(0, characters.Length)]);
     }
 }
 
--- Assets/Scripts/ShapeShift.cs
+++ Assets/Scripts/ShapeShift.cs
@@ -29,10 +29,11 @@ public class ShapeShift : MonoBehaviour {
 
         ////////////////Pour les debugs
 
-	    if (Input.GetButtonUp("ChangeForm"))
+	   
+       /*if (Input.GetButtonUp("ChangeForm"))
         {
             ChangeMesh(_gameObjectTest);
-        }
+        }*/
     }
 
 
