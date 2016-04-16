3c2b3bd248634a7b5a4d47ef529c5d74ad3db99f
Guillaume Laruelle <g.laruelle@student.isartdigital.com>
2016-04-16 20:02:48 +0200

    Modif Script ShapeShift

--- Assets/ShapeShift.cs
+++ Assets/ShapeShift.cs
@@ -5,13 +5,17 @@ using System.Collections.Generic;
 public class ShapeShift : MonoBehaviour {
 
     [SerializeField]
-    private List<Mesh> _meshList;
+    private GameObject _gameObjectTest;
 
     private MeshFilter _meshFilter;
 
     private Transform _currentTransform;
 
     private int _currentMesh = 0;
+    
+    private Vector3 _currentPos;
+
+    private Quaternion _currentRot;
 
 
 	// Use this for initialization
@@ -27,73 +31,53 @@ public class ShapeShift : MonoBehaviour {
 
 	    if (Input.GetButtonUp("ChangeForm"))
         {
-            _currentMesh = _currentMesh + 1;
-            Debug.Log(_currentMesh);
-            if (_currentMesh >= _meshList.Count)
-            {
-                _currentMesh = 0;
-            }
-            ChangeMesh(_currentMesh);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            _currentMesh = _currentMesh + 1;
-            Debug.Log(_currentMesh);
-            if (_currentMesh >= _meshList.Count)
-            {
-                _currentMesh = 0;
-            }
-            ChangeMesh(_currentMesh, new Vector3(this.transform.position.x + 1f, this.transform.position.y + 1f, this.transform.position.z + 1f));
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            _currentMesh = _currentMesh + 1;
-            Debug.Log(_currentMesh);
-            if (_currentMesh >= _meshList.Count)
-            {
-                _currentMesh = 0;
-            }
-            ChangeMesh(_currentMesh, new Vector3(this.transform.position.x + 1f, this.transform.position.y + 1f, this.transform.position.z + 1f),new Vector3 (this.transform.position.x +45f, this.transform.position.y, this.transform.position.z));
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            _currentMesh = _currentMesh + 1;
-            Debug.Log(_currentMesh);
-            if (_currentMesh >= _meshList.Count)
-            {
-                _currentMesh = 0;
-            }
-            ChangeMesh(_currentMesh, new Vector3(this.transform.position.x + 1f, this.transform.position.y + 1f, this.transform.position.z + 1f), new Vector3(this.transform.position.x + 45f, this.transform.position.y, this.transform.position.z), 2f);
+            ChangeMesh(_gameObjectTest);
         }
     }
 
-    public void ChangeMesh(int meshNum)
-    {
-        _meshFilter.mesh = _meshList[meshNum];
-    }
 
-    public void ChangeMesh(int meshNum, Vector3 position)
+    public void ChangeMesh(GameObject newObj)
     {
-        _meshFilter.mesh = _meshList[meshNum];
-        _currentTransform.position = position;
+        _currentRot = _currentTransform.rotation;
+        Destroy(this.transform.GetChild(0).gameObject);
+        GameObject objInst = Instantiate(newObj, Vector3.zero, _currentRot) as GameObject;
+        objInst.transform.parent = this.transform;
+        objInst.transform.position = Vector3.zero;
+        
+        
         
     }
 
-    public void ChangeMesh(int meshNum, Vector3 position, Vector3 rotation)
+    /*public void ChangeMesh(int meshNum, GameObject newObj)
     {
-        _meshFilter.mesh = _meshList[meshNum];
-        _currentTransform.position = position;
-        _currentTransform.rotation = Quaternion.Euler(rotation);
+        _currentPos = _currentTransform.position;
+        _currentRot = _currentTransform.rotation;
+
+        Instantiate(newObj, _currentPos, _currentRot);
     }
 
-    public void ChangeMesh(int meshNum, Vector3 position, Vector3 rotation, float scale)
+    public void ChangeMesh(int meshNum, GameObject newObj, Vector3 position)
     {
-        _meshFilter.mesh = _meshList[meshNum];
-        _currentTransform.position = position;
-        _currentTransform.rotation = Quaternion.Euler(rotation);
-        _currentTransform.localScale = new Vector3(scale, scale, scale);
+        _currentPos = _currentTransform.position;
+        _currentRot = _currentTransform.rotation;
+
+        Instantiate(newObj, _currentPos, _currentRot);
+
     }
+
+    public void ChangeMesh(int meshNum, GameObject newObj, Vector3 position, Vector3 rotation)
+    {
+        _currentPos = _currentTransform.position;
+        _currentRot = _currentTransform.rotation;
+
+        Instantiate(newObj, _currentPos, _currentRot);
+    }
+
+    public void ChangeMesh(int meshNum, GameObject newObj, Vector3 position, Vector3 rotation, float scale)
+    {
+        _currentPos = _currentTransform.position;
+        _currentRot = _currentTransform.rotation;
+
+        Instantiate(newObj, _currentPos, _currentRot);
+    }*/
 }
