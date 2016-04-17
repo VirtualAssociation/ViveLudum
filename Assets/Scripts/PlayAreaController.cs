bf17f62cfbdc338119593d8f2291a4436dee0dd7
Valentin Michalak <valentinmichalak@gmail.com>
2016-04-17 14:39:29 +0200

    Add Lose when enemies enter on play zone

---
+++ Assets/Scripts/PlayAreaController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAreaController : MonoBehaviour
+{
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.collider.tag != "DestructibleChild"){ return; }
+
+		Debug.Log("YOU LOSE");
+	}
+}
